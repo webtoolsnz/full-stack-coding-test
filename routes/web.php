9c9514ac62a2f58af8839c44720e872887e53637
Jonathan Guo <jonathan@webtools.co.nz>
2020-07-10 14:10:21 +1200

    :sparkles: Added basic auth scaffold.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\LoginController@showLoginForm');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
